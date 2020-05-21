05b83461d23be57948661f265638b7c2d347d061
thiagolsfortunato <thiagolsfortunato@hotmail.com>
2020-05-21 15:15:43 -0300

    ignore files

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for dir in $(ls | grep service) ; do
-  cd $dir
-  DIR_NAME=$(basename "$PWD")
-  cat "${DIR_NAME}.yaml" | sed "s/{{PROJECT_ID}}/${PROJECT_ID}/g" | kubectl apply -f -
-  cd ..
-done
